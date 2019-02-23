cfefd2d11615cf773b2b8dc69309169a3e901e92
crkteam <39040121+crkteam@users.noreply.github.com>
2019-02-23 19:54:13 +0800

    c
    c

---
+++ CrkJump/Assets/Game/Script/MainCharacter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainCharacter : MonoBehaviour
+{
+    [SerializeField]
+    Sprite left, right;
+    
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.GetComponent<Image>().sprite = left;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {   
+            jump_left();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            jump_right();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
+
+    void jump_right()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(500, 1500);
+        // 這裡播放動畫
+        gameObject.GetComponent<Animator>().SetTrigger("JumpR");
+
+//        gameObject.GetComponent<Image>().overrideSprite = right;
+    }
+    
+    void jump_left()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-500, 1500);
+        // 這裡播放動畫
+        gameObject.GetComponent<Animator>().SetTrigger("JumpL");
+        
+//        gameObject.GetComponent<Image>().overrideSprite = left;
+
+    }
+}
---
+++ CrkJump/Assets/Game/Script/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+//	public string 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-500,0);
+		}
+	}
+}
