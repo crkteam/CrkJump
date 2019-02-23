9f8b4dbdc1200dee490140fd3c5c5800b2f1134e
crkteam <39040121+crkteam@users.noreply.github.com>
2019-02-23 17:18:58 +0800

    crkJK
    第一次上傳

---
+++ CrkJump/Assets/Game/Meter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meter : MonoBehaviour
+{
+    private bool meter_switch = true;
+    private bool condition = true;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (meter_switch)
+        {
+            float value = gameObject.GetComponent<Slider>().value;
+
+            if (value >= 100)
+                condition = false;
+            if (value <= 0)
+                condition = true;
+
+            if (condition)
+                gameObject.GetComponent<Slider>().value += 1.5f;
+            else
+                gameObject.GetComponent<Slider>().value -= 1.5f;
+        }
+    }
+
+    public void meter_stop()
+    {
+        meter_switch = false;
+    }
+
+    public void meter_start()
+    {
+        meter_switch = true;
+    }
+}
